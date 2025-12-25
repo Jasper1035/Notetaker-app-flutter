37d5b94c3c53da1ba5358c25277506f297bd1106
Jasper1034 <104673426+Jasper1035@users.noreply.github.com>
2025-12-25 19:49:11 +0000

    notetaker app learnig
    learning from youtube tutorial

---
+++ notetaker/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:notetaker/constants.dart';
+import 'package:notetaker/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: 'Poppins',
+        scaffoldBackgroundColor: background,
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+          backgroundColor: Colors.transparent,
+          titleTextStyle: TextStyle(
+            color: primary,
+            fontSize: 32,
+            fontFamily: 'Fredoka',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
