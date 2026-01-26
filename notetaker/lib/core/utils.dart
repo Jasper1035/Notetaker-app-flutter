0fdc353c91d1415840a9aef9df3e0f1f2d3ff95f
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-01-26 16:18:17 +0000

    chore: apply .gitignore to remove build files

--- notetaker/lib/core/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-String toShortDate(int dateTime) => DateFormat(
-  'dd MMM, y',
-).format(DateTime.fromMicrosecondsSinceEpoch(dateTime));
-
-String toLongtDate(int dateTime) => DateFormat(
-  'dd MMMM y, hh:mm a',
-).format(DateTime.fromMicrosecondsSinceEpoch(dateTime));
